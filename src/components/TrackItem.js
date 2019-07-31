408fc2a7fb78e5f6133e02de4f9aeecfd75fb5ec
Tod Fleeman <rtoddf@me.com>
2019-07-31 18:41:48 -0400

    active ready - more work to do

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -3,16 +3,22 @@ import Moment from "react-moment";
 import "./trackItem.css";
 
 class TrackItem extends React.Component {
-  state = { isPlaying: true };
+  state = { isPlaying: true, isActive: true };
 
   onClick = e => {
     this.setState({ isPlaying: !this.state.isPlaying });
     this.props.onTrackPlay(e, this.props.track, this.state.isPlaying);
+    this.setState({ isActive: !this.state.isActive });
   };
 
   render() {
     return (
-      <div key={this.props.track.key} className="jukebox-card">
+      <div
+        key={this.props.track.key}
+        className={`jukebox-card ${
+          this.state.isActive ? "non-active" : "active"
+        }`}
+      >
         <div className="inner">
           <div className="song-artwork">
             <img
